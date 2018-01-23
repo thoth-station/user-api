88ecdd425252b43732c437f802f94b8b473312a5
Fridolin Pokorny <fridolin@redhat.com>
2018-01-23 13:44:51 +0100

    Remove unused configuration entry

--- thoth_user_api/configuration.py
+++ thoth_user_api/configuration.py
@@ -7,6 +7,4 @@ class Configuration:
     OPENSHIFT_API_URL = os.environ['OPENSHIFT_API_URL']
     OPENSHIFT_API_TOKEN = os.environ['OPENSHIFT_API_TOKEN']
     OPENSHIFT_PROJECT_NAME = os.environ['OPENSHIFT_PROJECT_NAME']
-
-    APP_SERVICE_PORT = os.getenv('APP_SERVICE_PORT', 34000)
     SWAGGER_YAML_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'swagger.yaml')
