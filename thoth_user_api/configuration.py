9c059f6a34e498eea59794032853d6ce78c49ff5
Fridolin Pokorny <fridolin@redhat.com>
2018-01-31 16:47:30 +0100

    Be able to supply API token to pod via env variables

--- thoth_user_api/configuration.py
+++ thoth_user_api/configuration.py
@@ -15,7 +15,7 @@ class Configuration:
     # Please provide explicitly.
     APP_SECRET_KEY = os.environ['THOTH_USER_API_APP_SECRET_KEY']
     KUBERNETES_API_URL = os.getenv('KUBERNETES_API_URL', 'https://kubernetes.default.svc.cluster.local')
-    KUBERNETES_API_TOKEN = os.getenv('KUBERNETES_API_TOKEN', _get_api_token())
+    KUBERNETES_API_TOKEN = os.getenv('KUBERNETES_API_TOKEN')
     THOTH_ANALYZER_NAMESPACE = os.environ['THOTH_ANALYZER_NAMESPACE']
     SWAGGER_YAML_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'swagger.yaml')
     THOTH_ANALYZER_HARD_TIMEOUT = int(os.getenv('THOTH_ANALYZER_HARD_TIMEOUT',
@@ -23,3 +23,6 @@ class Configuration:
     THOTH_RESULT_API_HOSTNAME = os.environ['THOTH_RESULT_API_HOSTNAME']
     THOTH_MIDDLEEND_POD_MEMORY_LIMIT = os.getenv('THOTH_MIDDLEEND_POD_MEMORY_LIMIT', '0.5Gi')
     THOTH_MIDDLEEND_POD_CPU_LIMIT = os.getenv('THOTH_MIDDLEEND_POD_CPU_LIMIT', '0.5')
+
+
+Configuration.KUBERNETES_API_TOKEN = Configuration.KUBERNETES_API_TOKEN or _get_api_token()
