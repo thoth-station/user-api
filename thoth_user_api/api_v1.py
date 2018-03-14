d5dbe1d3b5201b4cc03ab423b7099edb2ca0012b
Fridolin Pokorny <fridolin@redhat.com>
2018-03-14 15:22:28 +0100

    Refactoring, build logs retrieval endpoint

--- thoth_user_api/api_v1.py
+++ thoth_user_api/api_v1.py
@@ -1,15 +1,12 @@
 #!/usr/bin/env python3
 
-import json
-import os
-import re
 from itertools import islice
 
 from thoth.storages import AnalysisResultsStore
 from thoth.storages import SolverResultsStore
 from thoth.storages import BuildLogsStore
+from thoth.storages.base import StorageBase
 
-from .configuration import Configuration
 from .parsing import parse_log as do_parse_log
 from .utils import get_pod_log as do_get_pod_log
 from .utils import get_pod_status as do_get_pod_status
@@ -19,7 +16,6 @@ from .utils import run_pod
 from .utils import run_solver
 from .utils import run_sync
 
-_BUILDLOG_ID_RE = re.compile(r'[a-zA-Z0-9]+')
 PAGINATION_SIZE = 100
 
 
@@ -37,43 +33,6 @@ def analyze(image: str, analyzer: str, debug: bool=False, timeout: int=None,
         return {
             'error': str(exc),
             'parameters': params
-        }, 400
-
-
-def get_analyzer_result(document_id: str):
-    """Retrieve image analyzer result."""
-    try:
-        adapter = AnalysisResultsStore()
-        adapter.connect()
-        result = adapter.retrieve_document(document_id)
-        return result
-    except Exception as exc:
-        # TODO: handle 404 as a special case here
-        return {
-            'error': str(exc),
-            'parameters': {
-                'document_id': document_id
-            }
-        }, 400
-
-
-def list_analyzer_results(page: int=0):
-    """Retrieve image analyzer result."""
-    try:
-        adapter = AnalysisResultsStore()
-        adapter.connect()
-        result = adapter.get_document_listing()
-        # TODO: I'm not sure if Ceph returns objects in the same order each time.
-        # We will need to abandon this logic later anyway once we will be able to query results on data hub side.
-        return {
-            "results": list(islice(result, page*PAGINATION_SIZE, page*PAGINATION_SIZE + PAGINATION_SIZE)),
-            "page": page,
-            "page_size": PAGINATION_SIZE
-        }
-    except Exception as exc:
-        # TODO: some errors should be filtered out
-        return {
-            'error': str(exc),
         }, 400
 
 
@@ -107,43 +66,6 @@ def solve(solver: str, packages: dict, debug: bool=False, cpu_request: str=None,
         return {
             'error': str(exc),
             'parameters': params
-        }, 400
-
-
-def get_solver_result(document_id: str):
-    """Retrieve solver result."""
-    try:
-        adapter = SolverResultsStore()
-        adapter.connect()
-        result = adapter.retrieve_document(document_id)
-        return result
-    except Exception as exc:
-        # TODO: handle 404 as a special case here
-        return {
-            'error': str(exc),
-            'parameters': {
-                'document_id': document_id
-            }
-        }, 400
-
-
-def list_solver_results(page: int=0):
-    """Retrieve image analyzer result."""
-    try:
-        adapter = SolverResultsStore()
-        adapter.connect()
-        result = adapter.get_document_listing()
-        # TODO: I'm not sure if Ceph returns objects in the same order each time.
-        # We will need to abandon this logic later anyway once we will be able to query results on data hub side.
-        return {
-            "results": list(islice(result, page*PAGINATION_SIZE, page*PAGINATION_SIZE + PAGINATION_SIZE)),
-            "page": page,
-            "page_size": PAGINATION_SIZE
-        }
-    except Exception as exc:
-        # TODO: some errors should be filtered out
-        return {
-            'error': str(exc),
         }, 400
 
 
@@ -224,36 +146,72 @@ def post_buildlog(log_info: dict):
     adapter.connect()
     document_id = adapter.store_document(log_info)
 
-    # Duplicate storing also on PV for now.
-    content = json.dumps(log_info, sort_keys=True, indent=2)
-    document_path = os.path.join(Configuration.THOTH_BUILDLOGS_PERSISTENT_VOLUME_PATH, document_id + '.json')
-    with open(document_path, 'w') as output_file:
-        output_file.write(content)
-
     return {
-        'log_id': document_id
+        'document_id': document_id
     }, 202
 
 
-def get_buildlog(log_id: str):
-    """Retrieve the given buildlog."""
-    # adapter = BuildLogsStore()
-    # adapter.connect()
-    # document = adapter.retrieve_document(log_id)
-    if not _BUILDLOG_ID_RE.fullmatch(log_id):
+def list_buildlogs(page: int=0):
+    """List availabe build logs."""
+    return _do_listing(BuildLogsStore(), page)
+
+
+def list_analyzer_results(page: int=0):
+    """Retrieve image analyzer result."""
+    return _do_listing(AnalysisResultsStore(), page)
+
+
+def list_solver_results(page: int=0):
+    """Retrieve image analyzer result."""
+    return _do_listing(SolverResultsStore(), page)
+
+
+def _do_listing(adapter: StorageBase, page: int) -> tuple:
+    """Perform actual listing."""
+    try:
+        adapter.connect()
+        result = adapter.get_document_listing()
+        # TODO: I'm not sure if Ceph returns objects in the same order each time.
+        # We will need to abandon this logic later anyway once we will be able to query results on data hub side.
         return {
-            'error': 'Invalid buildlog identifier {!r}'.format(log_id),
-            'log_id': log_id
+            "results": list(islice(result, page*PAGINATION_SIZE, page*PAGINATION_SIZE + PAGINATION_SIZE)),
+            "page": page,
+            "page_size": PAGINATION_SIZE
+        }, 200
+    except Exception as exc:
+        # TODO: some errors should be filtered out
+        return {
+            'error': str(exc),
         }, 400
 
-    log_file = os.path.join(Configuration.THOTH_BUILDLOGS_PERSISTENT_VOLUME_PATH, log_id + '.json')
-    try:
-        with open(log_file, 'r') as build_log:
-            content = json.load(build_log)
-    except FileNotFoundError:
-        return {
-            'error': 'Buildlog file with id {!r} was not found'.format(log_id),
-            'log_id': log_id
-        }, 404
 
-    return content, 200
+def get_solver_result(document_id: str):
+    """Retrieve solver result."""
+    return _get_document(SolverResultsStore, document_id)
+
+
+def get_analyzer_result(document_id: str):
+    """Retrieve image analyzer result."""
+    return _get_document(AnalysisResultsStore, document_id)
+
+
+def get_buildlog(document_id: str):
+    """Retrieve the given buildlog."""
+    return _get_document(BuildLogsStore, document_id)
+
+
+def _get_document(adapter_class, document_id: str) -> tuple:
+    """Perform actual document retrieval."""
+    try:
+        adapter = adapter_class()
+        adapter.connect()
+        result = adapter.retrieve_document(document_id)
+        return result, 200
+    except Exception as exc:
+        # TODO: handle 404 as a special case here
+        return {
+            'error': str(exc),
+            'parameters': {
+                'document_id': document_id
+            }
+        }, 400
