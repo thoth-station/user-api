f02121ad148017607f0a3263b984b6badc348161
Fridolin Pokorny <fridolin@redhat.com>
2018-07-24 13:15:35 +0200

    Use OpenShift as a naming service

--- thoth_user_api/configuration.py
+++ thoth_user_api/configuration.py
@@ -28,28 +28,25 @@ class Configuration:
     """Configuration of user-facing API service."""
 
     APP_SECRET_KEY = os.environ['THOTH_USER_API_APP_SECRET_KEY']
-    SWAGGER_YAML_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)),  # Ignore PycodestyleBear (E501)
-                                     'swagger.yaml')
+    SWAGGER_YAML_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'swagger.yaml')
 
-    KUBERNETES_API_URL = os.getenv(
-        'KUBERNETES_API_URL', 'https://kubernetes.default.svc.cluster.local')
+    KUBERNETES_API_URL = os.getenv('KUBERNETES_API_URL', 'https://kubernetes.default.svc.cluster.local')
+    OPENSHIFT_API_URL = os.getenv('OPENSHIFT_API_URL', 'https://openshift.default.svc.cluster.local')
     KUBERNETES_VERIFY_TLS = bool(int(os.getenv('KUBERNETES_VERIFY_TLS', True)))
     KUBERNETES_API_TOKEN = os.getenv('KUBERNETES_API_TOKEN') or get_service_account_token()
 
     THOTH_MIDDLETIER_NAMESPACE = os.environ['THOTH_MIDDLETIER_NAMESPACE']
     THOTH_BACKEND_NAMESPACE = os.environ['THOTH_BACKEND_NAMESPACE']
-    THOTH_ANALYZER_HARD_TIMEOUT = int(os.getenv('THOTH_ANALYZER_HARD_TIMEOUT',
-                                                datetime.timedelta(hours=24).total_seconds()))  # Ignore PycodestyleBear (E501)
+    THOTH_INFRA_NAMESPACE = os.environ['THOTH_BACKEND_NAMESPACE']
+    THOTH_ANALYZER_HARD_TIMEOUT = int(
+        os.getenv('THOTH_ANALYZER_HARD_TIMEOUT', datetime.timedelta(hours=24).total_seconds())
+    )
     THOTH_RESULT_API_URL = os.environ['THOTH_RESULT_API_URL']
     THOTH_ADVISER_OUTPUT = THOTH_RESULT_API_URL + '/api/v1/adviser-result'
     THOTH_ANALYZER_OUTPUT = THOTH_RESULT_API_URL + '/api/v1/analysis-result'
     THOTH_SOLVER_OUTPUT = THOTH_RESULT_API_URL + '/api/v1/solver-result'
-    THOTH_MIDDLETIER_POD_MEMORY_LIMIT = os.getenv(
-        'THOTH_MIDDLETIER_POD_MEMORY_LIMIT', '0.5Gi')
-    THOTH_MIDDLETIER_POD_CPU_LIMIT = os.getenv(
-        'THOTH_MIDDLETIER_POD_CPU_LIMIT', '0.5')
-    THOTH_MIDDLETIER_POD_MEMORY_REQUEST = os.getenv(
-        'THOTH_MIDDLETIER_POD_MEMORY_REQUEST', '32Mi')
-    THOTH_MIDDLETIER_POD_CPU_REQUEST = os.getenv(
-        'THOTH_MIDDLETIER_POD_CPU_REQUEST', '0.1')
+    THOTH_MIDDLETIER_POD_MEMORY_LIMIT = os.getenv('THOTH_MIDDLETIER_POD_MEMORY_LIMIT', '0.5Gi')
+    THOTH_MIDDLETIER_POD_CPU_LIMIT = os.getenv('THOTH_MIDDLETIER_POD_CPU_LIMIT', '0.5')
+    THOTH_MIDDLETIER_POD_MEMORY_REQUEST = os.getenv('THOTH_MIDDLETIER_POD_MEMORY_REQUEST', '32Mi')
+    THOTH_MIDDLETIER_POD_CPU_REQUEST = os.getenv('THOTH_MIDDLETIER_POD_CPU_REQUEST', '0.1')
     THOTH_SECRET = os.environ['THOTH_SECRET']
