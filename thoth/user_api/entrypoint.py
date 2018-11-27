a6e0b32f4dc7b19eba695df597e8b5079c5a8d84
Christoph Görn <goern@redhat.com>
2018-11-27 10:58:59 +0100

    added prometheus metric export for all things REST
    Signed-off-by: Christoph Görn <goern@redhat.com>

--- thoth/user_api/entrypoint.py
+++ thoth/user_api/entrypoint.py
@@ -20,10 +20,11 @@
 import logging
 from datetime import datetime
 
-from flask import redirect
-from flask import jsonify
 import connexion
+
+from flask import redirect, jsonify
 from flask_script import Manager
+from prometheus_flask_exporter import PrometheusMetrics
 
 from thoth.common import SafeJSONEncoder
 from thoth.common import init_logging
@@ -38,32 +39,39 @@ from .configuration import Configuration
 app = connexion.App(__name__)
 application = app.app
 init_logging()
-_LOGGER = logging.getLogger('thoth.user_api')
+_LOGGER = logging.getLogger("thoth.user_api")
 
 app.add_api(Configuration.SWAGGER_YAML_PATH)
 application.json_encoder = SafeJSONEncoder
+metrics = PrometheusMetrics(application)
 manager = Manager(application)
+
 # Needed for session.
 application.secret_key = Configuration.APP_SECRET_KEY
 
+# static information as metric
+metrics.info("user_api_info", "User API info", version=thoth_user_api.__version__)
 
-@app.route('/')
+
+@app.route("/")
+@metrics.do_not_track()
 def base_url():
     """Redirect to UI by default."""
-    return redirect('api/v1/ui')
+    return redirect("api/v1/ui")
 
 
-@app.route('/api/v1')
+@app.route("/api/v1")
+@metrics.do_not_track()
 def api_v1():
     """Provide a listing of all available endpoints."""
     paths = []
 
     for rule in application.url_map.iter_rules():
         rule = str(rule)
-        if rule.startswith('/api/v1'):
+        if rule.startswith("/api/v1"):
             paths.append(rule)
 
-    return jsonify({'paths': paths})
+    return jsonify({"paths": paths})
 
 
 def _healthiness():
@@ -73,49 +81,52 @@ def _healthiness():
     adapter.connect()
     adapter.ceph.check_connection()
 
-    return jsonify({
-        'status': 'ready',
-        'version': thoth_user_api.__version__}
-    ), 200, {'ContentType': 'application/json'}
+    return jsonify({"status": "ready", "version": thoth_user_api.__version__}), 200, {"ContentType": "application/json"}
 
 
-@logger_setup('werkzeug', logging.WARNING)
-@logger_setup('botocore.vendored.requests.packages.urllib3.connectionpool', logging.WARNING)
-@app.route('/readiness')
+@logger_setup("werkzeug", logging.WARNING)
+@logger_setup("botocore.vendored.requests.packages.urllib3.connectionpool", logging.WARNING)
+@app.route("/readiness")
+@metrics.do_not_track()
 def api_readiness():
     """Report readiness for OpenShift readiness probe."""
     return _healthiness()
 
 
-@logger_setup('werkzeug', logging.WARNING)
-@logger_setup('botocore.vendored.requests.packages.urllib3.connectionpool', logging.WARNING)
-@app.route('/liveness')
+@logger_setup("werkzeug", logging.WARNING)
+@logger_setup("botocore.vendored.requests.packages.urllib3.connectionpool", logging.WARNING)
+@app.route("/liveness")
+@metrics.do_not_track()
 def api_liveness():
     """Report liveness for OpenShift readiness probe."""
     return _healthiness()
 
 
 @application.errorhandler(404)
+@metrics.do_not_track()
 def page_not_found(exc):
     """Adjust 404 page to be consistent with errors reported back from API."""
     # Flask has a nice error message - reuse it.
-    return jsonify({'error': str(exc)}), 404
+    return jsonify({"error": str(exc)}), 404
 
 
 @application.errorhandler(500)
+@metrics.do_not_track()
 def internal_server_error(exc):
     """Adjust 500 page to be consistent with errors reported back from API."""
     # Provide some additional information so we can easily find exceptions in logs (time and exception type).
     # Later we should remove exception type (for security reasons).
-    return jsonify({
-        'error': 'Internal server error occurred, please contact administrator with provided details.',
-        'details': {
-            'type': exc.__class__.__name__,
-            'datetime': datetime.utcnow().isoformat()
-        }
-    }), 500
+    return (
+        jsonify(
+            {
+                "error": "Internal server error occurred, please contact administrator with provided details.",
+                "details": {"type": exc.__class__.__name__, "datetime": datetime.utcnow().isoformat()},
+            }
+        ),
+        500,
+    )
 
 
-if __name__ == '__main__':
-    _LOGGER.info(f'Thoth User API v{thoth_user_api.__version__}')
+if __name__ == "__main__":
+    _LOGGER.info(f"Thoth User API v{thoth_user_api.__version__}")
     manager.run()
