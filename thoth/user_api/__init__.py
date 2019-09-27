b75b07584c67377afa2cd5a1992c8dc74d8f2d5a
Christoph Görn <goern@redhat.com>
2019-09-27 11:12:26 +0200

    added list_python_packages() and what is required to expose it via API
    Signed-off-by: Christoph Görn <goern@redhat.com>

--- thoth/user_api/__init__.py
+++ thoth/user_api/__init__.py
@@ -1,4 +1,4 @@
 """Thoth User API."""
 
-__name__ = 'thoth-user-api'
-__version__ = '0.6.0-dev'
+__name__ = "thoth-user-api"
+__version__ = "0.6.0-dev"
--- thoth/user_api/api_v1.py
+++ thoth/user_api/api_v1.py
@@ -325,7 +325,7 @@ def get_advise_python(analysis_id):
 
 
 def get_advise_python_log(analysis_id: str):
-    """Get adviser log."""
+    """Get adviser log.""" 
     return _get_job_log(locals(), "adviser-", Configuration.THOTH_BACKEND_NAMESPACE)
 
 
@@ -562,6 +562,28 @@ def list_buildlogs(page: int = 0):
     """List available build logs."""
     return _do_listing(BuildLogsStore, page)
 
+
+def get_info():
+    """Get information about Thoth deployment."""
+    return {
+        "deployment_name": os.getenv("THOTH_DEPLOYMENT_NAME"),
+        "version:": os.getenv("OPENSHIFT_BUILD_REFERENCE", "@dev"),
+        "s3_endpoint_url": os.getenv("THOTH_S3_ENDPOINT_URL"),
+        "dgraph_host": os.getenv("GRAPH_SERVICE_HOST"),
+        "amun_api_url": os.getenv("AMUN_API_URL"),
+        "frontend_namespace": os.getenv("THOTH_FRONTEND_NAMESPACE"),
+        "middletier_namespace": os.getenv("THOTH_MIDDLETIER_NAMESPACE"),
+        "backend_namespace": os.getenv("THOTH_BACKEND_NAMESPACE"),
+        "s3_bucket_prefix": os.getenv("THOTH_CEPH_BUCKET_PREFIX"),
+    }
+
+
+def list_python_packages():
+    """
+    Retrieve information from the Knowledge Graph with regards to 
+    Python Package Names and the number of distinct Versions.
+    """
+    return
 
 def _do_listing(adapter_class, page: int) -> tuple:
     """Perform actual listing of documents available."""
