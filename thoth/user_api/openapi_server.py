36a92b0c246f217dcdc8cb386c788f3fe3baeaa2
Fridolin Pokorny <fridolin@redhat.com>
2019-10-24 19:49:51 +0200

    Do not introduce fatal fail if database schema was removed

--- thoth/user_api/openapi_server.py
+++ thoth/user_api/openapi_server.py
@@ -34,6 +34,7 @@ from flask_cors import CORS
 from thoth.common import datetime2datetime_str
 from thoth.common import init_logging
 from thoth.storages import GraphDatabase
+from thoth.storages.exceptions import DatabaseNotInitialized
 from thoth.user_api import __version__
 from thoth.user_api.configuration import Configuration
 from thoth.user_api.configuration import init_jaeger_tracer
@@ -92,7 +93,13 @@ def before_request_callback():
     if method == "GET" and path == "/metrics":
         graph = GraphDatabase()
         graph.connect()
-        _API_GAUGE_METRIC.set(int(graph.is_schema_up2date()))
+        try:
+            _API_GAUGE_METRIC.set(int(graph.is_schema_up2date()))
+        except DatabaseNotInitialized as exc:
+            # This can happen if database is erased after the service has been started as we
+            # have passed readiness probe with this check.
+            _LOGGER.exception("Cannot determine database schema as database is not initialized: %s", str(exc))
+            _API_GAUGE_METRIC.set(0)
 
 
 @app.route("/")
