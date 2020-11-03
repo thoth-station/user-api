d3299114ccfe9cdf29d5ce15eb669d4b2d359337
Fridolin Pokorny <fridolin.pokorny@gmail.com>
2020-11-03 21:11:53 +0100

    Make black happy

--- thoth/user_api/openapi_server.py
+++ thoth/user_api/openapi_server.py
@@ -82,14 +82,7 @@ application = app.app
 
 # create metrics and manager
 metrics = PrometheusMetrics(
-    application,
-    group_by="endpoint",
-    excluded_paths=[
-        "/liveness",
-        "/readiness",
-        "/api/v1/ui",
-        "/api/v1/openapi",
-    ],
+    application, group_by="endpoint", excluded_paths=["/liveness", "/readiness", "/api/v1/ui", "/api/v1/openapi",],
 )
 manager = Manager(application)
 
