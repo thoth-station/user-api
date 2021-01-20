3dee68034f9aaa997314c6bb9de5b3efacdae620
Francesco Murdaca <fmurdaca@redhat.com>
2021-01-20 17:17:11 +0100

    Expose schema metric
    Signed-off-by: Francesco Murdaca <fmurdaca@redhat.com>

--- thoth/user_api/openapi_server.py
+++ thoth/user_api/openapi_server.py
@@ -120,8 +120,18 @@ class _GraphDatabaseWrapper:
 # (hence the wrapper logic).
 GRAPH = _GraphDatabaseWrapper()
 
+# custom metric to expose head revision from thoth-storages library
+schema_revision_metric = metrics.gauge(
+    "thoth_database_schema_revision_script",
+    "Thoth database schema revision from script",
+    labels={
+        "component": "user-api",
+        "revision": GraphDatabase().get_script_alembic_version_head(),
+    },
+)
 
 @application.before_request
+@schema_revision_metric
 def before_request_callback():
     """Register this callback, so it is run before each request to this service."""
     method = request.method
